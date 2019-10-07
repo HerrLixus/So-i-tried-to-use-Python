57507b69e9db744007f1956bf9932b6b7577b756
HerrLixus <56248346+HerrLixus@users.noreply.github.com>
2019-10-07 10:48:06 +0300

    дописал пару строк
    "норм" и "Норм"

---
+++ talk_bot.py
@@ -0,0 +1,62 @@
+def godb():
+    print('Ну ладно, пока.')
+
+def wud():
+    print("Чем занимаешься?")
+    phr2 = input()
+    print('Круто. как продвигается работа?')
+    phr3 = input()
+    if phr3 == "хорошо":
+        print("Найс. Удачи.")
+    elif phr3 == "Хорошо":
+        print("Найс. Удачи.")
+    elif phr3 == "нормально":
+        print("Найс. Удачи.")
+    elif phr3 == "Нормально":
+        print("Найс. Удачи.")
+    elif phr3 == "плохо":
+        print("Может, тебе стоит отдохнуть?")
+    elif phr3 == "Плохо":
+        print("Может, тебе стоит отдохнуть?")
+    else:
+        print('Я тебя не понял.')
+
+def hau():
+    print('Как дела?')
+    phr1 = input()
+    if phr1 == "хорошо":
+        print("Это хорошо.")
+        wud()
+    elif phr1 == "Хорошо":
+        print("Это хорошо.")
+        wud()
+    elif phr1 == "нормально":
+        print("Это хорошо.")
+        wud()
+    elif phr1 == "Нормально":
+        print("Это хорошо.")
+        wud()
+    elif phr1 == "плохо":
+        print("Очень жаль.")
+        wud()
+    elif phr1 == "Плохо":
+        print("Очень жаль.")
+        wud()
+    else:
+        print('Я тебя не понял.')
+
+
+def hello():
+    print('Привет!')
+    phr = input()
+    if phr == 'привет':
+        hau()
+    elif phr == 'Привет':
+        hau()
+    elif phr == 'пока':
+        godb()
+    elif phr == 'Пока':
+        godb()
+    else:
+        print('Я тебя не понял.')
+hello()
